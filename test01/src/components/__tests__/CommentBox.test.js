2b2545b55b6a315dbb80eb0ecb9b3b83f8a50355
Eduardo Santos <e3duardo@live.com>
2020-03-15 22:13:16 -0300

    improving CombonentBox tests

---
+++ test01/src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has a textarea and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('the textarea', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' },
+    });
+    wrapped.update();
+  });
+
+  it('has a textarea that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('when form is submitted, textarea gets emptied', () => {
+    // maybe this line are weak, if we don't have the previous test,
+    // we could put and expectation about new comment value of textarea
+
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+});
