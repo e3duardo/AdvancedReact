4d605035d2fdfd76de4cb629d1c9dadea20f745e
Eduardo Santos <e3duardo@live.com>
2020-03-16 11:21:49 -0300

    just a integration test

---
+++ test01/src/__tests__/integrations.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import moxios from 'moxios';
+import Root from 'Root';
+import App from 'components/App';
+
+beforeEach(function() {
+  moxios.install();
+  moxios.stubRequest('http://localhost:4000/comments', {
+    status: 200,
+    response: [
+      {
+        name: 'Fetched #1',
+      },
+      {
+        name: 'Fetched #2',
+      },
+    ],
+  });
+});
+
+afterEach(function() {
+  moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', done => {
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+
+  wrapped.find('.fetch-comments').simulate('click');
+
+  moxios.wait(() => {
+    wrapped.update();
+
+    expect(wrapped.find('li').length).toEqual(2);
+
+    done();
+    wrapped.unmount();
+  });
+});
