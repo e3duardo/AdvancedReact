202cd71d91e012b1328869fb4648cf63c5ef540b
Eduardo Santos <e3duardo@live.com>
2020-03-16 09:38:28 -0300

    finishing the last ComponentList tests

--- test01/src/Root.js
+++ test01/src/Root.js
@@ -3,6 +3,8 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from 'reducers';
 
-export default ({ children }) => {
-  return <Provider store={createStore(reducers, {})}>{children}</Provider>;
+export default ({ children, initialState = {} }) => {
+  return (
+    <Provider store={createStore(reducers, initialState)}>{children}</Provider>
+  );
 };
