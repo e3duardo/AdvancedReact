da049ef90c8672dd55a712360a38c5271382dc8f
Eduardo Santos <e3duardo@live.com>
2020-03-17 10:55:27 -0300

    finishes the state validator middleware

--- 03 - Middlewares with Redux/src/Root.js
+++ 03 - Middlewares with Redux/src/Root.js
@@ -3,12 +3,13 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
 import asyncMiddleware from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(asyncMiddleware)
+    applyMiddleware(asyncMiddleware, stateValidator)
   );
 
   return <Provider store={store}>{children}</Provider>;
