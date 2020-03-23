0c960653473087743da3f1fa78eb34b516be30b2
Eduardo Santos <e3duardo@live.com>
2020-03-23 12:42:01 -0300

    server setup

---
+++ 04 - Authentication Server/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import router from "./router";
+
+const app = express();
+
+// App Setup
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`🚀 Server listening at port ${port}`);
+});
